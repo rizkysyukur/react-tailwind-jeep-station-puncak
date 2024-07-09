ec745a8ff672d0dffea8db29cc1cd46c25112ddd
rizkysyukur <rizkysyukur10@gmail.com>
2024-07-10 05:08:18 +0700

    Install flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
