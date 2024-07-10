6c10c043100cb653c8f55720310757a395298dae
rizkysyukur <rizkysyukur10@gmail.com>
2024-07-10 22:20:39 +0700

    Update navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,20 +4,24 @@ import { useState } from "react";
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="bg-gray-800">
+    <nav className="bg-[#1B1B1B]">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <div className="relative flex items-center justify-between h-20">
+          <div className="flex-shrink-0 ps-3">
+            <a href="/" className="text-white text-3xl audiowide-regular">
+              <span className="text-[#F5B754]">J</span>SP
+            </a>
+          </div>
+          <div className="flex items-center">
             {/* Mobile menu button */}
             <button
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+              className="inline-flex items-center justify-center p-2 rounded-md text-[#F5B754] hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
               aria-controls="mobile-menu"
               aria-expanded={isOpen ? "true" : "false"}
               onClick={() => setIsOpen(!isOpen)}
             >
               <span className="sr-only">Open main menu</span>
-              {/* Icon when menu is closed */}
               {!isOpen ? (
                 <svg
                   className="block h-6 w-6"
@@ -35,7 +39,6 @@ function Header() {
                   />
                 </svg>
               ) : (
-                // Icon when menu is open
                 <svg
                   className="block h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
@@ -54,49 +57,11 @@ function Header() {
               )}
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0">
-              <a href="/" className="text-white text-lg font-bold">
-                Your Logo
-              </a>
-            </div>
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                <a
-                  href="/"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Home
-                </a>
-                <a
-                  href="/about"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  About
-                </a>
-                <a
-                  href="/services"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Services
-                </a>
-                <a
-                  href="/contact"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Contact
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
 
       {/* Mobile menu, show/hide based on menu state. */}
-      <div
-        className={`${isOpen ? "block" : "hidden"} sm:hidden`}
-        id="mobile-menu"
-      >
+      <div className={`${isOpen ? "block" : "hidden"}`} id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1">
           <a
             href="/"
